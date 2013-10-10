21d73dc18f743d99f5b7c174260658014f33931c
Joshua <jdong42@gmail.com>
2013-10-10 12:06:04 -0500

    The beginning of pong. No collision.

--- Linux/source/pong/pong.cpp
+++ Linux/source/pong/pong.cpp
@@ -6,103 +6,115 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32; //32-bit color mode
 
-const int CLIP_BASIC = 4;
-const int CLIP_MOUSEOVER = 0;
-const int CLIP_MOUSEOUT = 1;
-const int CLIP_MOUSEDOWN = 2;
-const int CLIP_MOUSEUP = 3;
-
-SDL_Surface *background = NULL;
-SDL_Surface *buttonSheet = NULL;
+SDL_Surface *ball_sheet = NULL;
+SDL_Surface *paddle_sheet = NULL;
 SDL_Surface *screen = NULL;
 
 SDL_Event event;
 
-SDL_Rect clips[4];
-
-class Button {
+class Ball {
     private:
-        SDL_Rect box;
-        SDL_Rect* clip;
-        float velocity;
-        float pos;
+        SDL_Rect clip;
+        float x_pos;
+        float y_pos;
+        float radius; //= 32
+        double x_velocity;
+        double y_velocity;
     public:
-        Button(int x, int y, int w, int h);
+        Ball(int x, int y, int r, double dx, double dy);
+        void update_position();
+        void show();
+};
+
+Ball::Ball(int x, int y, int r, double dx, double dy) {
+    x_pos = x;
+    y_pos = y;
+    radius = r;
+    x_velocity = dx;
+    y_velocity = dy;
+    clip.x = 0;
+    clip.y = 0;
+    clip.w = radius*2;
+    clip.h = radius*2;
+}
+
+void Ball::update_position() {
+    /* X position update. */
+    if (0 >= x_pos) {
+        x_pos = 0.01;
+        x_velocity *= -1;
+    } else if (x_pos >= SCREEN_WIDTH-radius) {
+        x_velocity *= -1;
+        x_pos = SCREEN_WIDTH-radius-0.01;
+    } else {
+        x_pos += x_velocity;
+    }
+    /* Y position update. */
+    if (0 >= y_pos) {
+        y_pos = 0.01;
+        y_velocity *= -1;
+    } else if (y_pos >= SCREEN_HEIGHT-radius) {
+        y_velocity *= -1;
+        y_pos = SCREEN_HEIGHT-radius-0.01;
+    } else {
+        y_pos += y_velocity;
+    }
+    printf("%f, %f\n", x_velocity, y_velocity);
+}
+
+class Paddle {
+    private:
+        SDL_Rect clip;
+        float x_pos;
+        float y_pos;
+        float length;
+        float width;
+        float velocity;
+    public:
+        Paddle(int x, int y, int len);
+        void update_position();
         void handle_events();
         void show();
 };
 
-void set_clips() {
-    clips[CLIP_MOUSEOVER].x = 0;
-    clips[CLIP_MOUSEOVER].y = 0;
-    clips[CLIP_MOUSEOVER].w = 320;
-    clips[CLIP_MOUSEOVER].h = 240;
-
-    clips[CLIP_MOUSEOUT].x = 320;
-    clips[CLIP_MOUSEOUT].y = 0;
-    clips[CLIP_MOUSEOUT].w = 320;
-    clips[CLIP_MOUSEOUT].h = 240;
-
-    clips[CLIP_MOUSEDOWN].x = 0;
-    clips[CLIP_MOUSEDOWN].y = 240;
-    clips[CLIP_MOUSEDOWN].w = 320;
-    clips[CLIP_MOUSEDOWN].h = 240;
-
-    clips[CLIP_MOUSEUP].x = 320;
-    clips[CLIP_MOUSEUP].y = 240;
-    clips[CLIP_MOUSEUP].w = 320;
-    clips[CLIP_MOUSEUP].h = 240;
-
-    clips[CLIP_BASIC].x = 0;
-    clips[CLIP_BASIC].y = 0;
-    clips[CLIP_BASIC].w = 16;
-    clips[CLIP_BASIC].h = 128;
-}
-
-Button::Button(int x, int y, int w, int h) {
-    box.x = x;
-    box.y = y;
-    box.w = w;
-    box.h = h;
+Paddle::Paddle(int x, int y, int len) {
+    x_pos = x;
+    y_pos = y;
+    length = 128;
+    width = 16;
     velocity = 0;
-    pos = box.y;
-    clip = &clips[CLIP_BASIC];
 }
 
-void Button::handle_events() {
+void Paddle::update_position() {
+    /* Velocity decay function. */
+    velocity *= 0.96;
+    /* Y position update. */
+    y_pos += velocity;
+    clip.x = 0;
+    clip.y = 0;
+    clip.w = width;
+    clip.h = length;
+}
+
+void Paddle::handle_events() {
     Uint8 *keystates = SDL_GetKeyState(NULL);
-    velocity *= 0.94;
     /* UP */
-    if(keystates[SDLK_k] || keystates[SDLK_UP]) {
+    if (keystates[SDLK_k] || keystates[SDLK_UP]) {
         if (velocity > -3) {
-            velocity -= 0.08;
+            velocity -= 0.06;
         }
     }
     /* DOWN */
-    if(keystates[SDLK_j] || keystates[SDLK_DOWN]) {
+    if (keystates[SDLK_j] || keystates[SDLK_DOWN]) {
         if (velocity < 3) {
-            velocity += 0.08;
+            velocity += 0.06;
         }
     }
-    pos += velocity;
-    box.y = pos+0.5;
-    printf("%f\n", velocity);
-}
-
-SDL_Surface *load_image (std::string filename) {
-    SDL_Surface* loadedImage = NULL;
-    SDL_Surface* optimizedImage = NULL;
-    loadedImage = IMG_Load(filename.c_str());
-    if (loadedImage != NULL) {
-        optimizedImage = SDL_DisplayFormat(loadedImage);
-        SDL_FreeSurface(loadedImage);
-        if (optimizedImage != NULL) {
-            SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY,
-                            SDL_MapRGB(optimizedImage->format,
-                                       0, 0xFF, 0xFF));
-        }
+    if (y_pos <= 0) {
+        y_pos = 0;
+    } else if (y_pos >= SCREEN_HEIGHT-length) {
+        y_pos = SCREEN_HEIGHT-length;
     }
-    return optimizedImage;
 }
 
 void apply_surface(int x, int y,
@@ -115,9 +127,28 @@ void apply_surface(int x, int y,
     SDL_BlitSurface(source, clip, destination, &offset);
 }
 
-void Button::show() {
-    SDL_BlitSurface(background, NULL, screen, NULL);
-    apply_surface(box.x, box.y, buttonSheet, screen, clip);
+void Ball::show() {
+    apply_surface(x_pos, y_pos, ball_sheet, screen, &clip);
+}
+
+void Paddle::show() {
+    apply_surface(x_pos, y_pos, paddle_sheet, screen, &clip);
+}
+
+SDL_Surface *load_image (std::string filename) {
+    SDL_Surface* loadedImage = NULL;
+    SDL_Surface* optimizedImage = NULL;
+    loadedImage = IMG_Load(filename.c_str());
+    if (loadedImage != NULL) {
+        optimizedImage = SDL_DisplayFormatAlpha(loadedImage);
+        SDL_FreeSurface(loadedImage);
+        if (optimizedImage != NULL) {
+            SDL_SetColorKey(optimizedImage, SDL_SRCCOLORKEY,
+                            SDL_MapRGB(optimizedImage->format,
+                                       0, 0x00, 0x00));
+        }
+    }
+    return optimizedImage;
 }
 
 int init() {
@@ -133,23 +164,24 @@ int init() {
     if(SDL_EnableUNICODE(1) == -1) {
         return false;
     }
-    SDL_WM_SetCaption("TTF Test", NULL);
+    SDL_WM_SetCaption("SDL Pong", NULL);
     return true;
 }
 
 void clean_up() {
-    SDL_FreeSurface(buttonSheet);
+    SDL_FreeSurface(ball_sheet);
+    SDL_FreeSurface(paddle_sheet);
     SDL_FreeSurface(screen);
     SDL_Quit();
 }
 
 bool load_files() {
-    background = load_image("background.png");
-    buttonSheet = load_image("paddle.png");
-    if (background == NULL) {
+    ball_sheet = load_image("ball.png");
+    paddle_sheet = load_image("paddle.png");
+    if (ball_sheet == NULL) {
         return false;
     }
-    if (buttonSheet == NULL) {
+    if (paddle_sheet == NULL) {
         return false;
     }
     return true;
@@ -163,10 +195,9 @@ int main(int argc, char* args[]) {
     if (load_files() == false) {
         return 1;
     }
-    set_clips();
-    Button myButton(30, 0, 100, 100);
+    Ball ball1(50, 50, 32, 0.4, 0.4);
+    Paddle player_paddle(30, 0, 256);
     while (!quit) {
-        myButton.handle_events();
         if (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN) {
                 /* Hotkey: 'q' for quit. */
@@ -177,9 +208,13 @@ int main(int argc, char* args[]) {
                 quit = true;
             }
         }
+        ball1.update_position(); 
+        player_paddle.update_position();
+        player_paddle.handle_events();
         SDL_FillRect(screen, &screen->clip_rect,
-                     SDL_MapRGB(screen->format, 0xFF,0xFF, 0xEE));
-        myButton.show(); 
+                     SDL_MapRGB(screen->format, 0x00, 0x00, 0x00));
+        ball1.show();
+        player_paddle.show(); 
         if (SDL_Flip(screen) == -1) {
             return 1;
         }
